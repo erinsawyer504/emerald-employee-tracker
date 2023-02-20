0f7da754a7197181e6aa7a77ad0eed8cde10295b
erinsawyer504 <erinsawyer504@gmail.com>
2023-02-19 20:38:12 -0500

    fixed an issue in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ VALUES ('IT'),
 INSERT INTO role (title, salary, department_id)
 VALUES ('Help Desk Associate', 40000, 1),
         ('Software Engineer', 100000, 1),
-        ('Customer Service Representative', 30000, 2),
+        ('Customer Service Rep', 30000, 2),
         ('Customer Serice Manager', 50000, 2),
         ('Human Resources Agent', 65000, 3),
         ('Human Resources Manager', 90000, 3),
@@ -15,11 +15,11 @@ VALUES ('Help Desk Associate', 40000, 1),
         ('Sales Manager', 85000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id)
-VALUES('Stefan', 'Salvatore', 4),
-        ('Elena', 'Gilbert', 6),
-        ('Tyler', 'Lockwood', 1),
-        ('Matt', 'Donovan', 5),
-        ('Caroline', 'Forbes', 7),
-        ('Katherine', 'Pierce', 2),
-        ('Bonnie', 'Bennett', 3),
-        ('Klaus', 'Mikealson', 8);
+VALUES('Stefan', 'Salvatore', 12),
+        ('Elena', 'Gilbert', 14),
+        ('Tyler', 'Lockwood', 9),
+        ('Matt', 'Donovan', 13),
+        ('Caroline', 'Forbes', 15),
+        ('Katherine', 'Pierce', 10),
+        ('Bonnie', 'Bennett', 11),
+        ('Klaus', 'Mikealson', 16);
